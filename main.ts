ab8c90d11ff84c58165cea42d2dacbd1b1242a95
reg17971 <81472942+reg17971@users.noreply.github.com>
2022-06-15 10:56:04 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,39 @@
+function led2 () {
+    if (turn_flag) {
+        basic.showLeds(`
+            . . # # #
+            . . # # .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # # #
+            . . . # #
+            . . . . #
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . # # . .
+            # # # . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            # . . . .
+            # # . . .
+            # # # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(100)
+    }
+}
 let turn_flag = false
+turn_flag = false
 let increase_speed_flag = 0
